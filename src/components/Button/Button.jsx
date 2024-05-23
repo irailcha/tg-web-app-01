6d5f5093e99648db01ef0b4a7a81c687e205b54c
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-05-23 15:22:46 +0300

    add components Button and Header

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Button = (props) => {
+  return (
+    <>
+      <button {...props} className={`button ${props.className}`}>
+        {props.children}
+      </button>
+    </>
+  );
+};
+
+export default Button;
