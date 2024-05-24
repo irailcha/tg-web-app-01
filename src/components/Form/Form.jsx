39a46294c6ef7e29983e6211d695fd836b638f34
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-05-24 17:03:43 +0300

    add styles for form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ import Button from "../Button/Button";
 const Form = () => {
   const [name, setName] = useState("");
   const [country, setCountry] = useState("");
-  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
   const [subject, setSubject] = useState("physical");
   const { tg } = useTelegram();
 
@@ -13,11 +13,11 @@ const Form = () => {
     const data = {
       name,
       country,
-      street,
+      city,
       subject,
     };
-    tg.onSendData(JSON.stringify(data));
-  }, [name, country, street, subject]);
+    tg.sendData(JSON.stringify(data));
+  }, [name, country, city, subject]);
 
   useEffect(() => {
     Telegram.WebApp.onEvent("mainButtonClicked", onSendData);
@@ -34,12 +34,12 @@ const Form = () => {
   }, []);
 
   useEffect(() => {
-    if (!country || !street) {
+    if (!country || !city) {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
     }
-  }, [country, street]);
+  }, [country, city]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
@@ -47,8 +47,8 @@ const Form = () => {
   const onChangeCountry = (e) => {
     setCountry(e.target.value);
   };
-  const onChangeStreet = (e) => {
-    setStreet(e.target.value);
+  const onChangeCity = (e) => {
+    setCity(e.target.value);
   };
 
   const onChangeSubject = (e) => {
@@ -56,7 +56,8 @@ const Form = () => {
   };
 
   return (
-    <div>
+    <div className="form-container">
+      <h1>Enter your details</h1>
       <form className="form">
         <label className="form-label" htmlFor="name">
           Name:
@@ -70,23 +71,29 @@ const Form = () => {
           value={name}
           onChange={onChangeName}
         />
+        <label className="form-label" htmlFor="name">
+          Country:
+        </label>
         <input
           type="text"
           id="country"
           name="country"
           className={"input"}
-          placeholder="Country"
+          placeholder="Ukraine"
           value={country}
           onChange={onChangeCountry}
         />
+        <label className="form-label" htmlFor="name">
+          City:
+        </label>
         <input
           type="text"
-          id="street"
-          name="street"
+          id="city"
+          name="city"
           className={"input"}
-          placeholder="Street"
-          value={street}
-          onChange={onChangeStreet}
+          placeholder="Dnipro"
+          value={city}
+          onChange={onChangeCity}
         />
 
         <label className={"label"} htmlFor="options">
