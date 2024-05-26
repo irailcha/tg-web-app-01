891e1b51cb98816408bbe7be6f6f5391a47b114d
Iryna Karpusenko <karpusenko.irina.13@gmail.com>
2024-05-26 17:14:33 +0300

    add products

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import "./ProductItem.css";
 import Button from "../Button/Button";
 
-const ProductItem = ({ product }) => {
+const ProductItem = ({ product, onAdd }) => {
   const { title, description, price, img } = product;
+
+  const onAddHandler = () => {
+    onAdd(product);
+  };
+
   return (
-    <div>
-      <img src={img} alt={title} />
+    <div className="item">
+      <img
+        className="img"
+        src={process.env.PUBLIC_URL + img}
+        alt={title}
+        width="50"
+      />
       <h3 className="title">{title}</h3>
       <p className="description">{description}</p>
-
-      <Button className="price">Price:{price}</Button>
+      <p className="price">
+        Price:<span>{price}</span>
+      </p>
+      <Button onClick={onAddHandler}>Add to basket</Button>
     </div>
   );
 };
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductItem from "../ProductItem/ProductItem";
 import products from "../../products.json";
+import Button from "../Button/Button";
+import { useTelegram } from "../../hooks/useTelegram";
+
+const getTotalPrice = (items = []) => {
+  return items.reduce((acc, item) => {
+    return (acc += item.price);
+  }, 0);
+};
 
 const ProductList = () => {
+  const { tg } = useTelegram();
+  const [addedProducts, setAddedProducts] = useState([]);
+
+  const onAdd = (product) => {
+    const isAdded = addedProducts.find((item) => item.id === product.id);
+
+    let newProd = [];
+    if (!isAdded) {
+      newProd = [...addedProducts, product];
+    } else {
+      newProd = addedProducts.filter((item) => item.id !== product.id);
+    }
+
+    setAddedProducts(newProd);
+
+    if (newProd.length === 0) {
+      tg.MainButton.hide();
+    } else {
+      tg.MainButton.show();
+      tg.MainButton.setParams({ text: `Buy ${getTotalPrice(newProd)}` });
+    }
+  };
+
   return (
     <div>
-      <ul className="listItem">
+      <ul className="list-product">
         {products.map((product) => (
-          <li className="item" key={product.id}>
-            <ProductItem product={product} />
+          <li key={product.id}>
+            <ProductItem product={product} onAdd={onAdd} />
           </li>
         ))}
       </ul>
